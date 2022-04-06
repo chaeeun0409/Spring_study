efb1980e0ec5786ae61e86854fb73b5d149a35ef
Chae Eun <81691990+chaeeun0409@users.noreply.github.com>
2022-04-06 11:45:32 +0900

    Setter DI 예제

---
+++ SetterDIPrj/src/main/java/kr/hs/study/main/MainClass.java
@@ -0,0 +1,27 @@
+package kr.hs.study.main;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import kr.hs.study.beans.TestBean;
+import kr.hs.study.beans.TestBean2;
+
+public class MainClass {
+
+   public static void main(String[] args) {
+      ClassPathXmlApplicationContext ctx=
+            new ClassPathXmlApplicationContext("kr/hs/study/config/config.xml");
+      
+      TestBean obj1=ctx.getBean("t1", TestBean.class);
+      obj1.Prn();
+  
+      
+      System.out.println("=========================================");
+      
+      TestBean2 obj2=ctx.getBean("t2", TestBean2.class);
+      obj2.Prn();
+      
+      ctx.close();
+
+   }
+
+}
