f30ab1f6357e50379d98618b0759e0a907101c3f
Chae Eun <81691990+chaeeun0409@users.noreply.github.com>
2022-04-09 19:41:09 +0900

    Person 예제

---
+++ Person/src/main/java/kr/hs/study/beans/TestBean.java
@@ -0,0 +1,14 @@
+package kr.hs.study.beans;
+
+public class TestBean {
+	private Person p1;
+	private Person p2;
+	
+	public TestBean(Person p1, Person p2, Person age, Person name) {
+		this.p1=p1;
+		this.p2=p2;
+	
+	}
+	
+	
+}
