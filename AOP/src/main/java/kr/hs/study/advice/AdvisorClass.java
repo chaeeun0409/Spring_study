5012ad3885d99cbe7df2e84f9c7806472dda78da
Chae Eun <81691990+chaeeun0409@users.noreply.github.com>
2022-04-19 11:38:06 +0900

    AOP 예제

---
+++ AOP/src/main/java/kr/hs/study/advice/AdvisorClass.java
@@ -0,0 +1,25 @@
+package kr.hs.study.advice;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+
+//횡단관심사가 들어갈 곳
+public class AdvisorClass {	
+	public void beforMethod() {
+		System.out.println("berformethod 호출");
+	}
+	public void afterMethod() {
+		System.out.println("afterMethod 호출");
+	}
+	public void aroundMethod(ProceedingJoinPoint pjp) throws Throwable {
+		System.out.println("aroundMethod 호출1");
+		pjp.proceed(); //실제 비지니스 로직 호출(method1)
+		System.out.println("aroundMethod 호출 2");
+	}
+	public void afterReturning() {
+		System.out.println("afterReturning 호출");
+	}
+	public void afterThrowing(Throwable e) {
+		System.out.println("afterThrowing 호출");
+		System.out.println("e"+e);
+	}
+}
